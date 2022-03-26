c196c6d3cdb4e0d181bb0e65ee1a7bb38c5b816b
alllex123456 <alextanase454@gmail.com>
2022-03-26 16:51:49 +0200

    fixed sorting mongo

--- pages/api/queueHandler.js
+++ pages/api/queueHandler.js
@@ -27,7 +27,11 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       await db.collection('translationsQueue').insertOne(newOrder);
-      const orders = await db.collection('translationsQueue').find().toArray();
+      const orders = await db
+        .collection('translationsQueue')
+        .find()
+        .sort({ deadline: 1 })
+        .toArray();
       res.status(200).json({ message: orders });
       connect.close();
     } catch (error) {
