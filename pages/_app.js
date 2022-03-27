ab8f4e90e0c66e4c3e1afd646d0e9e3f996f6746
alllex123456 <alextanase454@gmail.com>
2022-03-27 12:32:14 +0300

    added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
+import NextNProgress from 'nextjs-progressbar';
+import { Fragment } from 'react/cjs/react.production.min';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+      <NextNProgress />
+    </Fragment>
+  );
 }
 
 export default MyApp;
