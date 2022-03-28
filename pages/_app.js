3c2d7b4d91c8bf0d396e6f97e8e6ff5cbe95e46e
alllex123456 <alextanase454@gmail.com>
2022-03-28 14:53:34 +0300

    added head

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
 import NextNProgress from 'nextjs-progressbar';
-import { Fragment } from 'react/cjs/react.production.min';
+import Head from 'next/head';
+import { Fragment } from 'react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
+      <Head>
+        <title>Translations Queue</title>
+      </Head>
       <Component {...pageProps} />
       <NextNProgress />
     </Fragment>
