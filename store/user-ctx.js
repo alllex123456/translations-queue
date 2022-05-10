aeabf42546b35bf42dfa024420790c7c1a124456
alllex123456 <alextanase454@gmail.com>
2022-05-10 14:27:15 +0300

    fetch user info

--- store/user-ctx.js
+++ store/user-ctx.js
@@ -1,13 +1,23 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const UserContext = createContext({
   language: '',
 });
 
 export const UserContextProvider = (props) => {
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    fetch('/api/user/get-user')
+      .then((res) => res.json())
+      .then((data) => setUserInfo(data.message));
+  }, []);
+
   const initialValues = {
-    language: 'ro',
+    language: userInfo?.language,
   };
+
+  console.log(initialValues);
   return (
     <UserContext.Provider value={initialValues}>
       {props.children}
